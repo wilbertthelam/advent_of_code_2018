4fbc7f66edd8b04ee9c424094656b3773fd8cce4
David Lee <david.yongmin.lee@gmail.com>
2018-12-11 01:39:38 -0800

    Day 2 Problem 1 solution

--- day2/problem1.py
+++ day2/problem1.py
@@ -1,3 +1,18 @@
 import os
 
 with open('input.txt', 'r') as f:
+    read_data = f.readlines()
+    #go through each line to find checksum eligibility
+    twice = 0
+    thrice = 0
+    for line in read_data:
+        map = dict()
+        for letter in line:
+            map[letter] = map.get(letter, 0) + 1
+            #print(letter)
+        if 2 in map.values():
+            twice = twice + 1
+        if 3 in map.values():
+            thrice = thrice + 1
+    checksum = twice * thrice
+    print(checksum)
