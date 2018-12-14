a6ede2c35241f6c4277ccd557b85bda2a95f3d40
Wilbert Lam <wilbertthelam@gmail.com>
2018-12-13 20:42:35 -0800

    += you heathen

--- day1/problem1.py
+++ day1/problem1.py
@@ -5,5 +5,5 @@ with open('input.txt', 'r') as f:
     sum = 0
     for line in read_data:
         #print(line)
-        sum = sum + int(line)
+        sum += int(line)
     print("The sum is " + str(sum))
